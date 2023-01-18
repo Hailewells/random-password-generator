c7e25d7fc98e60ca656fd1cdb6b21c0cca8437d3
Hailewells <119069535+Hailewells@users.noreply.github.com>
2023-01-18 12:39:33 -0800

    Add files via upload

---
+++ random password.py
@@ -0,0 +1,11 @@
+import random
+lower_case='abcdefghijklmnopqrstuvwxyz'
+upper_case='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers='0123456789'
+symbols='!@#$%^&*\/+'
+
+use_for=lower_case+upper_case+numbers+symbols
+length_of_password=8
+password=" ".join(random.sample(use_for,length_of_password))
+
+print("your generated password is : ",password)
